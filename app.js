9ace2634725aed21c086b5fae5ea7295cd81819a
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2020-02-15 22:38:19 -0500

    updated logic and cleaned up instructions from CLI

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 // Step 1 collect employee info
 const collectEmployeeData = require('./lib/collectEmployeeData').collectEmployeeData;
-const createEmployee = require('./lib/teamMemberConstructor').createEmployee;
 const generateTeamHtml = require('./lib/generateTeamHtml').generateTeamHtml;
 
 const init = async () => {
     const inputs = await collectEmployeeData();
-    //createEmployee(inputs);
     generateTeamHtml(inputs);
 };
 
